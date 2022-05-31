0e04c5279959058f00d942f45b94c9285a988d5f
Carolina <carolinaruo@gmail.com>
2022-05-31 12:33:15 -0300

    first commit

---
+++ src/main/java/com/spring/blog/controller/BlogController.java
@@ -0,0 +1,55 @@
+package com.spring.blog.controller;
+
+import com.spring.blog.model.Post;
+import com.spring.blog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class BlogController {
+
+    @Autowired
+    BlogService blogService; //usando interface para qnd quiser mudar a impl dela, o código nao vai quebrar
+
+    //MVC; Model = Post; RETORNAR MODEL E A VIEW (o que o html vai renderizar)
+    @RequestMapping(value="/posts", method=RequestMethod.GET)
+    public ModelAndView getPosts() {
+        ModelAndView mv = new ModelAndView("posts"); //posts.html
+        List<Post> posts = blogService.findAll(); //vai retornar a lista de posts salvos no banco de dados
+        mv.addObject("posts", posts); //os objs java vao ser acessados atraves da palavra "posts"
+        return mv;
+    }
+
+    @RequestMapping(value="/posts/{id}", method=RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = blogService.findById(id);
+        mv.addObject("post", post);
+        return mv;
+    }
+
+    @RequestMapping(value="/newpost", method=RequestMethod.GET)
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @RequestMapping(value="newpost", method=RequestMethod.POST)
+        public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+            if(result.hasErrors()) {
+                return "redirect:/newpost";
+            }
+            post.setDate(LocalDate.now());
+            blogService.save(post);
+            return "redirect:/posts";
+        }
+}
---
+++ src/main/java/com/spring/blog/service/serviceImplement/BlogServiceImpl.java
@@ -0,0 +1,33 @@
+package com.spring.blog.service.serviceImplement;
+
+import com.spring.blog.model.Post;
+import com.spring.blog.repository.BlogRepository;
+import com.spring.blog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+//Mostrar ao spring que essa classe será um BEAN gerenciado por ele
+@Service
+public class BlogServiceImpl implements BlogService {
+
+    //Ponto de injeção do meu repository para conseguir utilizar as instancias desse repository
+    @Autowired
+    BlogRepository blogRepository;
+
+    @Override
+    public List<Post> findAll() {
+        return blogRepository.findAll();
+    }
+
+    @Override
+    public Post findById(long id) {
+        return blogRepository.findById(id).get();
+    }
+
+    @Override
+    public Post save(Post post) {
+        return blogRepository.save(post);
+    }
+}
